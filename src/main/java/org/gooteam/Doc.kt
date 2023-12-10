54c0ecb1b5706599dd52cad7101cca7c5efaf84a
hingler <jamieboy1337@gmail.com>
2023-12-09 16:10:57 -0800

    basic running code (need to provide token as cli arg)

--- src/main/java/org/gooteam/Doc.kt
+++ src/main/java/org/gooteam/Doc.kt
@@ -4,11 +4,12 @@ import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.exceptions.InvalidTokenException
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 // wrapper for the bot itself
 class Doc(
-    private val token: String
+    token: String
 ) : ListenerAdapter() {
 
     private lateinit var bot: JDA
@@ -17,7 +18,15 @@ class Doc(
         val builder = JDABuilder.createDefault(token)
         builder.addEventListeners(this)
         builder.setActivity(Activity.customStatus("gooning"))
-        bot = builder.build()
+
+        try {
+            bot = builder.build()
+
+            bot.awaitReady()
+            print("Connected!")
+        } catch (e: InvalidTokenException) {
+            print("Error encountered: $e")
+        }
     }
 
     override fun onMessageReceived(event: MessageReceivedEvent) {
