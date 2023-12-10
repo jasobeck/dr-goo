17ff5b17b233a6e15cbb51da936f0f34d2cc4a97
hingler <jamieboy1337@gmail.com>
2023-12-09 17:56:12 -0800

    update gateway intent

--- src/main/java/org/gooteam/Goo.java
+++ src/main/java/org/gooteam/Goo.java
@@ -3,7 +3,8 @@ package org.gooteam;
 public class Goo {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String arg = (args.length > 1 ? args[0] : "INVALID_TOKEN");
+        String arg = (args.length >= 1 ? args[0] : "INVALID_TOKEN");
+        System.out.println("token: " + arg);
         // this should be it for now
         Doc doc = new Doc(arg);
     }
